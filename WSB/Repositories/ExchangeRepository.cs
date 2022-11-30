d5e953ae4b87907df5c3864d812e5083478d7532
TshokiKubu <Baitshokikubu@gmail.com>
2022-11-30 03:10:25 -0800

    Add project files.

---
+++ WSB/Repositories/ExchangeRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using WSB.Models;
+
+namespace WSB.Repositories
+{
+    public class ExchangeRepository : IExchangeRepository
+    {
+        public ExchangeRate getCurrency(string cuurrency)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CurrencyConverter()
+        {
+
+        }
+
+        
+    }
+}
---
+++ WSB/Repositories/IExchangeRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WSB.Models;
+
+namespace WSB.Repositories
+{
+    public interface IExchangeRepository
+    {
+        ExchangeRate getCurrency(string cuurrency);
+    }
+}
